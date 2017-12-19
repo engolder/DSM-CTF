b396ca3dabef4640756e22d72bbfbb6354661724
Jun-Mo <yunedoopea610@gmail.com>
2017-12-19 19:35:59 +0900

    Finish sticky top bar

--- templates/static/script/script.js
+++ templates/static/script/script.js
@@ -1,14 +1,21 @@
 window.onload = function() {
     
 //    sidenav
-    sidenav = document.getElementsByClassName("sidenav");
-    sidenavOpenBtn = document.getElementsByClassName("sidenavOpenBtn");
-    sidenavCloseBtn = document.getElementsByClassName("sidenavCloseBtn");
+    let sidenav = document.getElementsByClassName("sidenav");
+    let sidenavOpenBtn = document.getElementsByClassName("sidenavOpenBtn");
+    let sidenavCloseBtn = document.getElementsByClassName("sidenavCloseBtn");
     
     sidenavOpenBtn[0].onclick = function() {
-        sidenav[0].classList.add("sidenav-open");
+        sidenav[0].classList.add("sidenav-active");
     }
     sidenavCloseBtn[0].onclick = function() {
-        sidenav[0].classList.remove("sidenav-open");
+        sidenav[0].classList.remove("sidenav-active");
+    }
+    
+//    stickynav
+    let stickyNav = document.getElementsByClassName("stickyNav");
+    let stickyNavList = document.getElementsByClassName("stickyNavList");
+    stickyNav[0].onclick = function() {
+        stickyNavList[0].classList.toggle("stickyNavList-active");
     }
 }
